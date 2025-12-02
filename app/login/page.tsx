82fcaf628527dcf1c57ab91418df8ce8f49d0fb7
Geetha P S <geethaps2001@gmail.com>
2025-12-02 21:04:42 +0530

    ha

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-
-import { Suspense } from "react";
 import LoginPageInner from "@/components/LoginPageInner";
 
 export default function LoginPage() {
-  return (
-    <Suspense fallback={<div className="p-6 text-center">Loading...</div>}>
-      <LoginPageInner />
-    </Suspense>
-  );
+  return <LoginPageInner />;
 }
